908eca316bdae35dde1103d86858aa68f3c432e3
Sebastian Castro <sebas.a.castro@gmail.com>
2026-01-25 21:22:15 -0500

    Add percentiles

--- experiments/benchmarks/README.md
+++ experiments/benchmarks/README.md
@@ -1,5 +1,10 @@
 # Benchmarks
 
+The basic benchmarks are directly modified from the [`dora-benchmark`](https://github.com/dora-rs/dora-benchmark) repository.
+
+Here, messages containing variable-size array data are sent between two processes.
+The latency between sending and receiving the messages are measured using wall clock time.
+
 ## Retriever Benchmarks
 
 ```bash
@@ -10,7 +15,7 @@ pixi run python experiments/benchmarks/benchmark_retriever.py --backend dora
 
 ```bash
 pixi run -e ros build
-pixi run -e ros benchmarks_python
+pixi run -e ros benchmark_python
 ```
 
 ## Plot results
--- experiments/benchmarks/plot_benchmark_results.py
+++ experiments/benchmarks/plot_benchmark_results.py
@@ -4,6 +4,7 @@ Script to plot benchmarking results.
 This by default looks at .csv files found in the experiments/benchmark/results folder.
 """
 
+import ast
 from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -16,12 +17,24 @@ results_dir = current_dir / "results"
 csv_files = list(results_dir.glob("*.csv"))
 
 # Plot the results for each file.
+lower_percentile = 10
+upper_percentile = 90
+
 plt.figure()
 
 legends = []
 for file in csv_files:
     df = pd.read_csv(file)
-    plt.plot(df["size"], df["latency_ns"] / 1000.0, marker="o")
+    latency_ns_arrays = df["latency_ns"].apply(ast.literal_eval)
+    max_len = max(len(a) for a in latency_ns_arrays)
+    latency_ms = np.array(
+        [np.pad(a, (0, max_len - len(a)), constant_values=np.nan) for a in latency_ns_arrays]
+    ) / 1000.0
+
+    median = np.nanpercentile(latency_ms, 50, axis=1)
+    lower = np.nanpercentile(latency_ms, lower_percentile, axis=1)
+    upper = np.nanpercentile(latency_ms, upper_percentile, axis=1)
+    plt.errorbar(df["size"], median, yerr=[median - lower, upper - median], fmt="o-", capsize=5)
     legends.append(df["name"][0])
 
 ax = plt.gca()
@@ -30,10 +43,11 @@ exponents = [6, 8, 12, 16, 20, 24]
 ticks = 2 ** np.array(exponents)
 ax.set_xticks(ticks)
 ax.set_xticklabels([fr"$2^{{{e}}}$" for e in exponents])
+plt.yscale("log")
 
 plt.xlabel("Message Size (bytes)")
 plt.ylabel("Latency (ms)")
-plt.title("Latency vs Message Size")
+plt.title(f"Latency vs Message Size ({lower_percentile}-{upper_percentile} percentile)")
 plt.legend(legends)
 plt.grid(True, which="both")
 plt.show()
